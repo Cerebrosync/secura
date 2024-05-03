9583e17544e83aa71c3fd6ae6b6f6cf1d96addc4
cerebrosync <cerebrosync@gmail.com>
2024-05-03 11:20:22 +0530

    upload hoja

---
+++ src/main/java/com/example/secura/SecuraApplication.java
@@ -0,0 +1,13 @@
+package com.example.secura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuraApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/secura/preventor.java
@@ -0,0 +1,4 @@
+package com.example.secura;
+
+public class preventor {
+}
